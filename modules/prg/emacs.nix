3c255a1fb03661c30552ede483387af80079dc18
vanishingideal <vanishingideal@protonmail.com>
2024-12-25 13:34:12 +0100

    fix(emacs): remove lsp, add flycheck, take things a bit slowly

--- modules/prg/emacs.nix
+++ modules/prg/emacs.nix
@@ -11,8 +11,6 @@
       with epkgs; [
         use-package
         evil
-        lsp-mode
-        lsp-ui
         org-bullets
         gruvbox-theme
         yasnippet
@@ -24,6 +22,7 @@
         nov
         fzf
         elfeed
+        flycheck
         org-roam
         emacsql
         emacsql-sqlite
@@ -37,166 +36,154 @@
       ];
 
     extraConfig = ''
-      ;;; Basic Performance Tweaks
-      (setq gc-cons-threshold 100000000)            ; Increase garbage collection threshold
-      (setq read-process-output-max (* 1024 1024))  ; Increase read process output
+           ;;; Basic Performance Tweaks
+           (setq gc-cons-threshold 100000000)            ; Increase garbage collection threshold
+           (setq read-process-output-max (* 1024 1024))  ; Increase read process output
 
-      ;;; Package Management
-      (setq use-package-always-ensure t)   ; Auto-install packages
+           ;;; Package Management
+           (setq use-package-always-ensure t)   ; Auto-install packages
 
-      ;;; Org-bullets
-      (require 'org-bullets)
-      (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+           ;;; Org-bullets
+           (require 'org-bullets)
+           (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
-      ;;; Basic UI Settings
-      (setq inhibit-startup-message t)     ; Disable startup message
-      (setq inhibit-startup-screen t)      ;
-      (scroll-bar-mode -1)                 ; Disable visible scrollbar
-      (horizontal-scroll-bar-mode -1)      ; Disable horizontal scrollbar
-      (tool-bar-mode -1)                   ; Disable the toolbar
-      (tooltip-mode -1)                    ; Disable tooltips
-      (menu-bar-mode -1)                   ; Disable the menu bar
-      (set-fringe-mode '(8 . 0))           ; Set left fringe to 8 pixels
-      (show-paren-mode 1)                  ; Show matching parentheses
-      (global-display-line-numbers-mode t) ; Turns on line numbers for all buffers
-      (setq display-line-numbers-type 'relative)
-      (setq ring-bell-function 'ignore)    ; Who wants audible text editors?
-      (setq-default truncate-lines t)      ; Prevent soft wrapping
-      (setq-default fill-column 79)
-      (setq-default auto-fill-mode t)
+           (use-package flycheck
+             :ensure t
+      :init (global-flycheck-mode))
 
-      ;;; Fonts
-      (set-face-attribute 'default nil :font "JetBrains Mono" :height 105)
-      (setq face-font-rescale-alist '((".*" . 1.0)))
-      (setq-default antialiasing nil)
+           ;;; Basic UI Settings
+           (setq inhibit-startup-message t)     ; Disable startup message
+           (setq inhibit-startup-screen t)      ;
+           (scroll-bar-mode -1)                 ; Disable visible scrollbar
+           (horizontal-scroll-bar-mode -1)      ; Disable horizontal scrollbar
+           (tool-bar-mode -1)                   ; Disable the toolbar
+           (tooltip-mode -1)                    ; Disable tooltips
+           (menu-bar-mode -1)                   ; Disable the menu bar
+           (set-fringe-mode '(8 . 0))           ; Set left fringe to 8 pixels
+           (show-paren-mode 1)                  ; Show matching parentheses
+           (global-display-line-numbers-mode t) ; Turns on line numbers for all buffers
+           (setq display-line-numbers-type 'relative)
+           (setq ring-bell-function 'ignore)    ; Who wants audible text editors?
+           (setq-default truncate-lines t)      ; Prevent soft wrapping
+           (setq-default fill-column 79)
+           (setq-default auto-fill-mode t)
+
+           ;;; Fonts
+           (set-face-attribute 'default nil :font "JetBrains Mono" :height 105)
+           (setq face-font-rescale-alist '((".*" . 1.0)))
+           (setq-default antialiasing nil)
 
 
-      ;;; Indentation
-      (setq-default indent-tabs-mode nil)  ; use spaces, not tabs
-      (setq-default tab-width 2)           ; set tab width to 2 spaces
-      (setq-default standard-indent 2)     ; default indentation
+           ;;; Indentation
+           (setq-default indent-tabs-mode nil)  ; use spaces, not tabs
+           (setq-default tab-width 2)           ; set tab width to 2 spaces
+           (setq-default standard-indent 2)     ; default indentation
 
-      ;;; Yasnippet
-      (use-package yasnippet
-        :ensure t
-        :config
-        (yas-global-mode 1))
+           ;;; Yasnippet
+           (use-package yasnippet
+             :ensure t
+             :config
+             (yas-global-mode 1))
 
-      ;;; Company
-      (use-package company
-        :hook (prog-mode . company-mode)
-        :config
-        (setq company-minimum-prefix-length 1
-              company-idle-delay 0.0
-              company-backends '((company-lsp company-files company-capf)))) ; ensure lsp backend
+           ;;; Company
+           (use-package company
+             :hook (prog-mode . company-mode)
+             :config
+             (setq company-minimum-prefix-length 1
+                   company-idle-delay 0.0
+                   company-backends '(('ac-js2-company company-files company-capf)))) ;
 
-      ;;; LSP Configuration
-      (use-package lsp-mode
-        :ensure t
-        :hook ((js2-mode . lsp)
-               (typescript-mode . lsp))
-        :commands lsp
-        :config
-        (setq lsp-headerline-breadcrumb-enable-diagnostics nil))
+           ;;; Evil Mode
+           (use-package evil
+             :config
+             (evil-mode 1))
 
-      (use-package lsp-ui
-        :commands lsp-ui-mode
-        :config
-        (setq lsp-ui-doc-enable t
-              lsp-ui-doc-position 'at-point
-              lsp-ui-sideline-show-code-actions t))
+           ;;; Emmet Configuration
+           (require 'emmet-mode)
+           (add-hook 'sgml-mode-hook 'emmet-mode)
+           (add-hook 'css-mode-hook  'emmet-mode)
+           (add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2)))
+           (define-key emmet-mode-keymap (kbd "TAB") 'emmet-expand-line)
+           (setq emmet-move-cursor-between-quotes t)
+           (add-to-list 'emmet-jsx-major-modes 'js-mode)
 
-      ;;; Evil Mode
-      (use-package evil
-        :config
-        (evil-mode 1))
+           ;;; JavaScript Configuration
+           (use-package js2-mode
+             :ensure t
+             :mode "\\.js\\'"
+             :hook ((js2-mode . yas-minor-mode)
+                    (js2-mode . company-mode)
+                    (js2-mode . flycheck-mode)))
 
-      ;;; Emmet Configuration
-      (require 'emmet-mode)
-      (add-hook 'sgml-mode-hook 'emmet-mode)
-      (add-hook 'css-mode-hook  'emmet-mode)
-      (add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2)))
-      (define-key emmet-mode-keymap (kbd "TAB") 'emmet-expand-line)
-      (setq emmet-move-cursor-between-quotes t)
-      (add-to-list 'emmet-jsx-major-modes 'js-mode)
+           (setenv "NODE_PATH" "/usr/local/lib/node_modules")
 
-      ;;; JavaScript Configuration
-      (use-package js2-mode
-        :ensure t
-        :mode "\\.js\\'"
-        :hook ((js2-mode . yas-minor-mode)
-               (js2-mode . company-mode)
-               (js2-mode . lsp)))
+           ;;; Lean4 Mode
+           (use-package lean4-mode
+             :commands (lean4-mode))
 
-      (setenv "NODE_PATH" "/usr/local/lib/node_modules")
+           ;;; Dired Configuration
+           (add-hook 'dired-mode-hook 'dired-hide-details-mode)
 
-      ;;; Lean4 Mode
-      (use-package lean4-mode
-        :commands (lean4-mode))
+           ;;; Elfeed Configuration
+           (defun load-elfeed-feeds-from-file (file)
+             (global-set-key (kbd "C-x w") 'elfeed)
+             (setq elfeed-feeds (load-elfeed-feeds-from-file "~/.emacs.d/feeds.txt")))
 
-      ;;; Dired Configuration
-      (add-hook 'dired-mode-hook 'dired-hide-details-mode)
+           ;;; Org-journal
+           (require 'org-journal)
+           (setq org-journal-dir "~/org/journal/")
+           (setq org-journal-date-format "%A, %d %B %Y")
+           (global-set-key (kbd "C-c C-j") 'org-journal-new-entry)
 
-      ;;; Elfeed Configuration
-      (defun load-elfeed-feeds-from-file (file)
-        (global-set-key (kbd "C-x w") 'elfeed)
-        (setq elfeed-feeds (load-elfeed-feeds-from-file "~/.emacs.d/feeds.txt")))
+           ;;; Org-roam Configuration
+           (use-package org-roam
+             :ensure t
+             :custom
+             (org-roam-completion-everywhere t)
+             (org-roam-directory "/home/vanishingideal/.org-roam")
+             :bind (:map org-mode-map
+                         ("C-M-i" . completion-at-point))
+             :config
+             (org-roam-setup))
 
-      ;;; Org-journal
-      (require 'org-journal)
-      (setq org-journal-dir "~/org/journal/")
-      (setq org-journal-date-format "%A, %d %B %Y")
-      (global-set-key (kbd "C-c C-j") 'org-journal-new-entry)
+           ;; Org-roam keybindings
+           (global-set-key (kbd "C-c n l") 'org-roam-buffer-toggle)
+           (global-set-key (kbd "C-c n f") 'org-roam-node-find)
+           (global-set-key (kbd "C-c n i") 'org-roam-node-insert)
 
-      ;;; Org-roam Configuration
-      (use-package org-roam
-        :ensure t
-        :custom
-        (org-roam-completion-everywhere t)
-        (org-roam-directory "/home/vanishingideal/.org-roam")
-        :bind (:map org-mode-map
-                    ("C-M-i" . completion-at-point))
-        :config
-        (org-roam-setup))
+           ;;; Theme Configuration
+           (use-package gruvbox-theme
+             :config
+             (load-theme 'gruvbox-dark-hard t)
+             :custom
+             (gruvbox-theme-use-bold-keywords t)
+             (gruvbox-theme-use-bold-builtins t)
+             (gruvbox-theme-use-italic-comments t))
 
-      ;; Org-roam keybindings
-      (global-set-key (kbd "C-c n l") 'org-roam-buffer-toggle)
-      (global-set-key (kbd "C-c n f") 'org-roam-node-find)
-      (global-set-key (kbd "C-c n i") 'org-roam-node-insert)
+           ;;; FZF
+           (use-package fzf
+             :bind
+             (("C-c ff" . fzf)             ; general fuzzy find
+             ("C-c fg" . fzf-git)          ; fuzzy search in git repo
+             ("C-c fG" . fzf-git-grep)     ; git-aware grep
+             ("C-c fr" . fzf-recentf)      ; fuzzy find recent files
+             ("C-c fd" . fzf-directory))   ; directory search
+             :config
+               (setq fzf/args "-x --color bw --print-query --margin=1,0 --no-hscroll"
+               fzf/executable "fzf"
+               fzf/git-grep-args "-i --line-number %s"
+               fzf/grep-command "grep -nrH"
+               fzf/position-bottom t
+               fzf/window-height 15))
 
-      ;;; Theme Configuration
-      (use-package gruvbox-theme
-        :config
-        (load-theme 'gruvbox-dark-hard t)
-        :custom
-        (gruvbox-theme-use-bold-keywords t)
-        (gruvbox-theme-use-bold-builtins t)
-        (gruvbox-theme-use-italic-comments t))
+           ;;; JS-specific indentation
+           (setq js-indent-level 2)
 
-      ;;; FZF
-      (use-package fzf
-        :bind
-        (("C-c ff" . fzf)             ; general fuzzy find
-        ("C-c fg" . fzf-git)          ; fuzzy search in git repo
-        ("C-c fG" . fzf-git-grep)     ; git-aware grep
-        ("C-c fr" . fzf-recentf)      ; fuzzy find recent files
-        ("C-c fd" . fzf-directory))   ; directory search
-        :config
-          (setq fzf/args "-x --color bw --print-query --margin=1,0 --no-hscroll"
-          fzf/executable "fzf"
-          fzf/git-grep-args "-i --line-number %s"
-          fzf/grep-command "grep -nrH"
-          fzf/position-bottom t
-          fzf/window-height 15))
+           ;;; Epub reader
+           (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
 
-      ;;; JS-specific indentation
-      (setq js-indent-level 2)
-
-      ;;; Epub reader
-      (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
-
-      ;;; Launch envrc
-      (envrc-global-mode)
+           ;;; Launch envrc
+           (envrc-global-mode)
     '';
   };
 }
